2a5e2f8f140ab3e2b537cc93f0392695b5186772
DasLixou <einlixou@gmail.com>
2022-06-23 19:42:03 +0200

    🪟 Add titlebar to window and some styling

---
+++ Cliff.Design/Components/TitleBar.axaml.cs
@@ -0,0 +1,22 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace Cliff.Design.Components
+{
+    public partial class TitleBar : UserControl
+    {
+        public TitleBar()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void CloseInteraction(object sender, RoutedEventArgs e) => ((Window)VisualRoot).Close();
+    }
+}
